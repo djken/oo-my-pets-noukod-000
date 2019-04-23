c0086102d02b7813888adceeaa2c08bdba0c8941
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-23 17:28:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,59 +5,73 @@ class Owner
   @@all = []
   @@pets = {:fishes => [], :dogs => [], :cats => []}
 
-  def initialize(specie)
-    @species = specie
+  def initialize(species)
+    @species = species
     @name = name
-    all << self
+    @@all << self
   end
 
   def self.all
-    @@owners
-  end
-
-  def self.reset_all
-    all.clear
+    @@all
   end
 
   def self.count
-    all.size
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
   end
 
   def say_species
-    "I am a #{species}."
+    "I am a #{@species}."
   end
 
-  def buy_fish(pet)
-    pets[:fishes] << Fish.new(pet)
+  def pets
+    @@pets
   end
 
-  def buy_cat(pet)
-    pets[:cats] << Cat.new(pet)
+  def buy_fish(fish)
+    @@pets[:fishes] << Fish.new(fish)
   end
 
-  def buy_dog(pet)
-    pets[:dogs] << Dog.new(pet)
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
   end
 
   def walk_dogs
-    pets[:dogs].each {|dog| dog.mood = "happy"}
+    @@pets[:dogs].each do |dog|
+      dog.mood = 'happy'
+    end
   end
 
   def play_with_cats
-    pets[:cats].each {|cat| cat.mood = "happy"}
+    @@pets[:cats].each do |cat|
+      cat.mood = 'happy'
+    end
   end
 
   def feed_fish
-    pets[:fishes].each {|fish| fish.mood = "happy"}
-  end
-
-  def sell_pets
-    pets.values.flatten.each {|pet| pet.mood = "nervous"}
-    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@pets[:fishes].each do |fish|
+      fish.mood = 'happy'
+    end
   end
 
   def list_pets
-    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
+  def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
   end
 
 end
